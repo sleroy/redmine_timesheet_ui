b04870f6d2f1e7afb7b866bc8ebbc42fe7eec77d
Leroy Sylvain <sleroy0@gmail.com>
2016-07-07 09:54:32 +0000

    First design

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Redmine : Timesheet Entry' });
+});
+/* GET Daily input */
+router.get('/', function(req, res, next) {
+  res.render('daily', { title: 'Daily timesheet' });
+});
+
+/* GET Activity input */
+router.get('/', function(req, res, next) {
+  res.render('daily', { title: 'Daily timesheet' });
+});
+
+/* GET Dashboard input */
+router.get('/', function(req, res, next) {
+  res.render('dashboard', { title: 'Dashboard' });
 });
 
 module.exports = router;
